846716f154cf5ae158d4711c8f2f45fedd330c2c
GreenpeaceKT <68542998+GreenpeaceKT@users.noreply.github.com>
2021-06-10 01:08:59 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+const Discord = require('discord.js');
+const { prefix, token } = require('./config.json');
 
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('message', message => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if (command === 'ping') {
+		message.channel.send('Pong.');
+	} else if (command === 'beep') {
+		message.channel.send('Boop.');
+	}
+	
+});
+
+client.login(token);
