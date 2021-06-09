cd28b9cdc235f3a252c64fad4f5d2332f3294183
GreenpeaceKT <68542998+GreenpeaceKT@users.noreply.github.com>
2021-06-10 01:22:44 +0900

    Update index.js

--- index.js
+++ index.js
@@ -5,12 +5,22 @@ const { prefix, token } = require('./config.json');
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 
 	client.commands.set(command.name, command);
+}
+
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args, client));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args, client));
+	}
 }
 
 client.once('ready', () => {
